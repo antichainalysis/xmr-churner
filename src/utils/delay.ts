994270a0fa2433a3df45e0334c99c8f39978e042
antichainalysis <varloughjessika@gmail.com>
2025-01-25 11:21:28 +0000

    formatting + jsdocs delay.ts

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,11 +1,15 @@
-import { randomRange } from "./random";
+import { randomRange } from './random';
 
+/**
+ * Random delay within the given range.
+ * @param {number[]} delayRange - Minimum and maximum delay in milliseconds
+ */
 export function randomDelay(delayRange: [number, number]) {
-    return new Promise((r) => {
-        const ms = randomRange(delayRange);
+  return new Promise((r) => {
+    const ms = randomRange(delayRange);
 
-        console.log("waiting", Math.floor(ms / (60 * 1000)), "minutes");
+    console.log('waiting', Math.floor(ms / (60 * 1000)), 'minutes');
 
-        setTimeout(r, ms);
-    });
+    setTimeout(r, ms);
+  });
 }
