ee254636cc175e230d3bb4b59eb085ed301fe0ee
antichainalysis <varloughjessika@gmail.com>
2025-01-25 11:21:52 +0000

    formatting + jsdocs random.ts

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,8 @@
-export function randomRange(range: [number, number]) {
-    return Math.floor(Math.random() * (range[1] - range[0]) + range[0]);
+/**
+ * Generate a random number with range
+ * @param {number[]} range - The start and end of the range.
+ * @returns {number} Random number within specified range.
+ */
+export function randomRange(range: [number, number]): number {
+  return Math.floor(Math.random() * (range[1] - range[0]) + range[0]);
 }
